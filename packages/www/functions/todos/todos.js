9f91596b19dd3086082c7f0d0700680153c4b5fd
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-04-16 21:50:58 +0500

    Save Todos to FaunaDB

--- packages/www/functions/todos/todos.js
+++ packages/www/functions/todos/todos.js
@@ -1,5 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
-
+const faunadb = require("faunadb");
+const q = faunadb.query;
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET_KEY });
 const typeDefs = gql`
   type Query {
     todos: [Todo]
@@ -30,16 +32,38 @@ const todos = [
 const resolvers = {
   Query: {
     todos: async () => {
-      return todos;
+      //Get todos
+      const result = await client.query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection('todos'))),
+          q.Lambda(x => {
+            return q.Get(x);
+          })
+        ));
+      return result.data.map(todo => {
+        return { ...todo.data, id: todo.ref.id };
+      });
     },
   },
   Mutation: {
     addTodo: async (_, { text, completed }) => {
-      return {
-        id: new Date().toISOString(),
-        text,
-        completed,
-      };
+      //save todo to FaunaDB
+      
+      try {
+        const result = await client.query(
+          q.Create(
+            q.Collection("todos"),
+            { data: { text, completed } }
+          )
+        );
+        return {
+          id: result.ref.id,
+          text,
+          completed,
+        };
+      } catch (e) {
+        console.log(e);
+      }
     },
     removeTodo: async (_, { id }) => {
       return {
